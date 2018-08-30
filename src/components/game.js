5e5073dec447c5f2761316b18cd2596d774b7999
Jeff Maxwell <maxjeffwell@gmail.com>
2018-08-30 14:08:20 -0400

    added state

--- src/components/game.js
+++ src/components/game.js
@@ -5,14 +5,28 @@ import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
+export default class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            feedback: 'Make your guess!',
+            count: 3,
+            guesses: [50, 75, 60]
+
+        }
+    }
+
+    render() {
+
+
+        return (
+            <div>
+                <Header/>
+                <GuessSection feedback="Make your guess!"/>
+                <GuessCount count={3}/>
+                <GuessList guesses={[10, 15, 25]}/>
+            </div>
+        );
+    }
 }
 
